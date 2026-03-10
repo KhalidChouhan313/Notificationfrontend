1d6b0d9dc29eaa0e3e8fcbdc57c79603baa30314
khalid chouhan <khalidchuhan7886@gmail.com>
2026-03-10 14:12:18 +0500

    fix

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  // Local development backend URL
-  SERVER_URL: 'http://localhost:3000',
+  SERVER_URL: 'https://alert-notification.vercel.app/',
   production: false,
-  VAPID_PUBLIC_KEY:
-    'BO1ZuzlxzYyYiByevlPJoJb9CSu8Ij3L303xzulESGZ7-N4JVE78ZudhVdelv609u4G24hG4WOzIxXdzRv3UXIo'
+  VAPID_PUBLIC_KEY: 'BO1ZuzlxzYyYiByevlPJoJb9CSu8Ij3L303xzulESGZ7-N4JVE78ZudhVdelv609u4G24hG4WOzIxXdzRv3UXIo'
 };
