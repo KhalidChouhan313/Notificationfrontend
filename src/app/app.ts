7b5d652f20f4673abd5b7ce6e40043834da0d3cf
khalid chouhan <khalidchuhan7886@gmail.com>
2026-03-10 13:55:38 +0500

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NotificationComponent } from "./notification/notification";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NotificationComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('alert-notification');
+}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  // Local development backend URL
+  SERVER_URL: 'http://localhost:3000',
+  production: false,
+  VAPID_PUBLIC_KEY:
+    'BO1ZuzlxzYyYiByevlPJoJb9CSu8Ij3L303xzulESGZ7-N4JVE78ZudhVdelv609u4G24hG4WOzIxXdzRv3UXIo'
+};
